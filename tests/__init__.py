c5616ae9cf82fd66c08335731dc06cc45333d893
Rhytah <ritanamono@gmail.com>
2018-10-21 22:53:06 +0300

    refactor code to update tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,19 @@
 from storeapi.views import app
+import json
 from storeapi.models import Product, products
 import unittest
 
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.test_client = app.test_client()
+        self.app = app
+        self.app.config['TESTING'] = True
+        self.token=''
+        self.auth_data = {
+            'username': 'admin',
+            'password': 'pass'
+        }
+        
         self.request_data={
             "productId":1,
             "productName":"Foam",
@@ -16,6 +25,15 @@ class BaseTestCase(unittest.TestCase):
             "created_by":"myself",
             "details":"Mattress material"
         }
+
+    def test_user_login(self):
+        
+        response = self.test_client.post('/api/v1/login',
+                                    data=json.dumps(self.auth_data),
+                                    content_type='application/json'
+                                    )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('access_token', str(response.data))
      
 if __name__ == "__main__":
     unittest.main()
--- tests/test_requests.py
+++ tests/test_requests.py
@@ -1,53 +1,87 @@
 from tests import BaseTestCase
 import json
 from flask import jsonify
+from flask_jwt_extended import JWTManager, create_access_token
 
 class RequestTestCase(BaseTestCase):
 
-    def test_add_product(self):
+    def test_add_product_without_token(self):
         response = self.test_client.post(
-            '/api/v1/admin/products', data=json.dumps(self.request_data), content_type='application/json'
+            '/api/v1/products', data=json.dumps(self.request_data), content_type='application/json'
         )
-        self.assertEqual(response.status_code,200)
-        self.assertIn(
-            'Product Foam successfully added', str(response.data)
-        )
-    def test_add_product_nonadmin(self):
-        response = self.test_client.post(
-        '/api/v1/products', data=json.dumps(self.request_data), content_type='application/json'
-        )
-        self.assertEqual(response.status_code,405)
+        self.assertEqual(response.status_code,401)
+        # self.assertIn(
+        #     'Product Foam successfully added', str(response.data)
+        # )
+    def test_admin_can_add_product(self): 
+        response = self.test_client.post('/api/v1/login',
+                                    data=json.dumps(self.auth_data),
+                                    content_type='application/json'
+                                    )
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('access_token', str(response.data))
         
+        login_response = response.json
+        self.token = login_response['access_token']
+        response = self.test_client.post('/api/v1/products',
+                            data=json.dumps(self.request_data),
+                            content_type='application/json',
+                            headers={'Authorization':
+                                        'Bearer {}'.format(self.token)})
+      
+        self.assertEqual(response.status_code, 200)
+    
     def test_fetch_products(self):
         response = self.test_client.get( 
-            '/api/v1/products', data=json.dumps(self.request_data), content_type = 'application/json')
+            '/api/v1/products', data=json.dumps(self.sale_data), content_type = 'application/json')
         self.assertEqual(response.status_code,200)
     
     def test_fetch_single_product(self):
         response=self.test_client.post(
-            'api/va/admin/products', data=json.dumps(self.request_data), content_type = 'application/json')
+            'api/v1/products', data=json.dumps(self.request_data), content_type = 'application/json')
         response = self.test_client.get(
             '/api/v1/products/1',  content_type='application/json')
         self.assertEqual(response.status_code,200)
         
 
 
-    def test_add_sale_order(self):    
-        response = self.test_client.post(
-            '/api/v1/attendant/sales', data=json.dumps(self.sale_data), content_type='application/json'
-        )
+    def test_add_sale_order(self):
+        with self.app.app_context():
+            token = create_access_token('attendant')
+            headers = {'Authorization': f'Bearer {token}'}
+            response = self.test_client.post(
+                '/api/v1/sales',
+                headers=headers,
+                content_type='application/json'
+            )
+            return(response.status)
         self.assertEqual(response.status_code,200)
         self.assertIn(
             'You have successfully created a sale order', str(response.data)
         )
     def test_fetch_single_sale_order(self):
-        response = self.test_client.get(
-            '/api/v1/sales/1', data=json.dumps(self.sale_data), content_type='application/json'
-        )
+        with self.app.app_context():
+            token = create_access_token('admin')
+            headers = {'Authorization': f'Bearer {token}'}
+            response = self.test_client.get(
+                '/api/v1/sales/1',
+                headers=headers,
+                content_type='application/json'
+            )
+            return(response.status)
+        self.assertEqual(200, response.status_code)
+       
         self.assertEqual(response.status_code,200)
 
     def test_fetch_all_sale_orders(self):
-        response = self.test_client.get( 
-            '/api/v1/admin/sales', data=json.dumps(self.sale_data), content_type = 'application/json')
-        self.assertEqual(response.status_code,200)
         
+        with self.app.app_context():
+            token = create_access_token('admin')
+            headers = {'Authorization': f'Bearer {token}'}
+            response = self.test_client.get(
+                '/api/v1/sales',
+                headers=headers,
+                content_type='application/json'
+            )
+            return(response.status)
+        self.assertEqual(200, response.status_code)
