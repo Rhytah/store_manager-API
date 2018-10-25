6c5877cde9cf9c9985bb135162bdbdd8dfa58a2e
Rhytah <ritanamono@gmail.com>
2018-10-26 01:31:17 +0300

    refactor tests

--- tests/test_products.py
+++ tests/test_products.py
@@ -22,7 +22,7 @@ class RequestTestCase(BaseTestCase):
                 content_type='application/json'
             )
             return(response.status)
-        # self.assertIn('Access denied, Log in as admin to add Products', str(response.data))
+        
     
     def test_admin_can_add_product(self): 
         with self.app.app_context():
@@ -34,7 +34,7 @@ class RequestTestCase(BaseTestCase):
                 headers=headers,
                 content_type='application/json')
             return(response.status)
-        # self.assertIn("Product Foam successfully added", str (response.data))
+       
     
     def test_fetch_products(self):
         response = self.test_client.get( 
@@ -57,9 +57,11 @@ class RequestTestCase(BaseTestCase):
                 '/api/v1/products',
                 headers=headers,
                 content_type='application/json')
-            return(response.status)        
+            return(response.status)       
+        
         response = self.test_client.post('/api/v1/products/1',
                             content_type='application/json',
                             data=json.dumps(self.request_data)
                             )      
+        self.assertEqual(response.status_code,200)
         self.assertIn("You have fetched product",str(response.data))
